b5f9bb7b0733a618f42e1e2c204f3c01e44a2876
Lucy Randewich <lucy@Lucys-MacBook-Air.local>
2021-12-01 17:20:30 +0000

    adding benchmarking function

---
+++ golClient/gol-skeleton/distributor_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"uk.ac.bris.cs/gameoflife/gol"
+)
+
+// Benchmark applies the filter to the 512x512 image b.N times.
+// The time taken is carefully measured by go.
+// The b.N repetition is needed because benchmark results are not always constant.
+func BenchmarkFilter(b *testing.B) {
+	// Disable all program output apart from benchmark results
+	os.Stdout = nil
+
+	var params gol.Params
+	params.ImageWidth = 512
+	params.ImageHeight = 512
+	params.Turns = 500
+	// For-loop to run 5 sub-benchmarks, with 1, 2, 4, 8 and 16 workers.
+	//for threads := 1; threads <= 512; threads*=2 {
+		fmt.Println("running with", 1, "threads")
+		b.Run(fmt.Sprintf("%d_workers", 1), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				params.Threads = 1
+				keyPresses := make(chan rune, 10)
+				events := make(chan gol.Event, 1000)
+				go gol.Run(params, events, keyPresses)
+				complete := false
+				for !complete {
+					event := <-events
+					switch event.(type) {
+					case gol.FinalTurnComplete:
+						complete = true
+					}
+				}
+			}
+		})
+	//}
+}

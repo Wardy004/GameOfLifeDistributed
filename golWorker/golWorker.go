5dc178bc0851039d76a75624a0637c838c3b9612
Lucy Randewich <lucy@Lucys-MacBook-Air.local>
2021-12-01 12:22:54 +0000

    removing comments

--- golWorker/golWorker.go
+++ golWorker/golWorker.go
@@ -197,7 +197,6 @@ func (s *GameOfLife) ProcessWorld(req stubsBrokerToWorker.Request, res *stubsBro
 			<-RowExchange
 			// Update liveCellsCounts at end of turn
 			liveCellCounts = append(liveCellCounts, countCells(req))
-			fmt.Println("liveCellsCounts are", liveCellCounts)
 			cpyWorld = copySlice(oWorld)
 		}
 	}
